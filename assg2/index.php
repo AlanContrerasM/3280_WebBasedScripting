350ff7cd7ff0b8ace31582848efdf31e19648bab
Alan Contreras <alanrcontrerasm@gmail.com>
2021-11-27 15:00:04 -0800

    Convert into class based file

--- assg2/index.php
+++ assg2/index.php
@@ -3,7 +3,7 @@
 require "lib.php";
 display_html_head("Algebraic Expressions");
 $degree = 2;  // the degree of an expression. Just change this degree and the whole program works based on the 
-$num_of_terms = 2 * $degree + 3;  // number of terms generated
+// $num_of_terms = 2 * $degree + 3;  // number of terms generated not needed anymore
 
 
 //1. depending if first time loading, ideally should be a function, for it to be cleaner...
@@ -17,13 +17,15 @@ if('POST' == $_SERVER['REQUEST_METHOD']){
         print '</li></ul>';
         display_form($inputs['my_degree']);
     }else{
-        global $degree, $num_of_terms;
+        global $degree;
         $degree = $inputs['my_degree'];
-        $num_of_terms = 2 * $degree + 3;
+
+        $expr = new Expression($degree);
+        $expr->generate();
+
+
         display_form($degree);
-        $expr = generate_expression($degree);
-        $expr_str = stringify_expression($expr);
-        display_expression("The Generated Expression:", $expr_str);
+        display_expression("The Generated Expression:", $expr->stringify());
         //simplify it here and store it in $expr_simplified_str
         // I set $expr_simplified_str="" because you must complete the code and set $expr_simplified_str to the simplified string.
         $expr_simplified_str ="";
@@ -169,3 +171,122 @@ function validate_form(){
     
     return array($errors, $inputs);
 }
+
+
+
+class Term{
+    public $coefficient;
+    public $exponent;
+
+    
+}
+
+class Expression{
+    const coefficient_min = -20;
+    const coefficient_max = 20;
+    public $terms;
+    public $degree;
+    public $num_of_terms;
+
+    public function __construct($degree){
+        $this->degree = $degree;
+        $this->num_of_terms = 2 * $this->degree + 3;
+        $this->terms = array();
+    }
+
+    public function generate(){
+        $degree = $this->degree;
+        $excluded = [0];
+        do {
+            // $this->terms = null;
+            $this->terms = array();
+            
+
+            for ($i = 0; $i < $this->num_of_terms; $i++) {
+                do {
+                    $coefficient = rand(self::coefficient_min, self::coefficient_max);
+                } while (in_array($coefficient, $excluded));
+                $exponent = rand(0, $degree);
+                //create new term, and assign values
+                $newTerm = new Term;
+                $newTerm->coefficient = $coefficient;
+                $newTerm->exponent = $exponent;
+                //add to array
+                $this->terms[] = $newTerm;
+            }
+        } while (!$this->meets_requirements($this->terms, $degree));  // re-generate expression if it does not meet the requirement
+        
+        
+        return $this->terms;
+    }
+
+    public function stringify(){
+        $expression = "";
+        $terms = $this->terms;
+        // strigify the first term as it is different that the other trms- the sign (- or +) is connected to the number, also if there is only 
+        // one term, no + or - sign is needed after 
+        if ($terms[0]->coefficient != 0) {
+            if ($terms[0]->exponent == 0) {
+                $expression .= $terms[0]->coefficient;
+            } else if ($terms[0]->coefficient == 1) {
+                $expression .= stringify_term($terms[0]->exponent);;
+            } else if ($terms[0]->coefficient == -1) {
+                $expression .= "-" . stringify_term($terms[0]->exponent);;
+            } else {
+                $expression .= $terms[0]->coefficient . stringify_term($terms[0]->exponent);
+            }
+        }
+        // strigify the rest of the terms
+        for ($i = 1; $i < count($terms); $i++) {
+            if ($terms[$i]->coefficient == 0) {
+                continue;
+            } else if ($terms[$i]->coefficient < 0) {
+                $expression .= " - ";
+            } else if ($terms[$i]->coefficient > 0) {
+                $expression .= " + ";
+            }
+            if ($terms[$i]->exponent == 0) {
+                $expression .= abs($terms[$i]->coefficient);
+            } else if (abs($terms[$i]->coefficient) == 1) {
+                $expression .= stringify_term($terms[$i]->exponent);;
+            } else {
+                $expression .= abs($terms[$i]->coefficient) . stringify_term($terms[$i]->exponent);
+            }
+        }
+        
+        return $expression;
+
+        
+    }
+
+    public function meets_requirements(){
+        
+
+        for ($deg = $this->degree; $deg >= 0; $deg--) {
+            $isValid = false;
+            $counter = 0;
+            for ($i = 0; $i < $this->num_of_terms; $i++) {
+                if ($this->terms[$i]->exponent == $deg) {
+                    $counter++;
+                    if ($counter == 2) {
+                        $isValid = true;
+                        break;
+                    }
+                }
+            }
+            if (!$isValid) {
+                break;
+            }
+        }
+        return $isValid;
+    }
+
+    public function simplify(){
+
+        return ;
+    }
+
+    
+}
+
+?>
